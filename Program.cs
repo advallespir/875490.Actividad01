ec11c100b2c0b64cc2d20659e70ad49fd8656d5f
ADVallespir <advallespir@gmail.com>
2021-03-17 23:20:01 -0300

    V1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _875490.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
